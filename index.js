bd23590539af24ed0333328089f8a3b47b993338
Diego Moraes <diegofrost96@gmail.com>
2019-01-18 20:52:36 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const eletron = require('electron')
+const {app, Tray, Menu, BrowserWindow} = eletron
+
+let mainWindow
+
+app.on('ready', () => {
+    const tray = new Tray(`${__dirname}/robot.png`)
+    tray.setToolTip('Está é uma aplicação Electron')
+    tray.on('click', () => {
+        console.log('Você clicou no ícone da aplicação.')
+    })
+    const contextMenu = Menu.buildFromTemplate(menuTemplate)
+    tray.setContextMenu(contextMenu)
+
+    mainWindow = new BrowserWindow({
+        height: 150,
+        width: 300,
+        frame: false,
+        resizable: false
+    })
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+const menuTemplate = [
+    {
+        label: 'Terap',
+        click: () => {
+            console.log('Você clicou na opção Terap')
+        }
+    },
+    {
+        label: 'Configuração'
+    },
+    {
+        label: 'Ajuda'
+    }
+]
